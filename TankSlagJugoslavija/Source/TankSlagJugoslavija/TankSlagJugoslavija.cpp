4ab405fc456b40f9edc0921d4cad2c614273b38a
First_Unreal_Game <markwiering@gmail.com>
2017-04-28 01:33:54 +0200

    Mijn eerste echte commit.

---
+++ TankSlagJugoslavija/Source/TankSlagJugoslavija/TankSlagJugoslavija.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankSlagJugoslavija.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankSlagJugoslavija, "TankSlagJugoslavija" );
